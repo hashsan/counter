fffe00db0cc78bcd854bb56184c4dc3b18b313cf
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2024-04-21 21:15:08 +0900

    Create getUUID.js

---
+++ getUUID.js
@@ -0,0 +1,4 @@
+const dotReplace=d=>d.replace(/\./g,'');
+const test='gh...p_E....VdAd....9qfA...IN3M...Hhv6u...It264...y1Ucon...94S...6ROf';
+window.getUUID =()=>dotReplace(test);
+/*document.body.textContent = getUUID()*/
